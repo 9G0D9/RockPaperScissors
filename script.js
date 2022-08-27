4a2ad7fc930c46793bb1a405e9db6199858b9fdd
Ghassen <dhiebghassen@gmail.com>
2022-08-27 19:08:37 +0100

    create the playRound function

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
- ;
+;
 
 function getComputerChoice() {
-    const CHOICES=["Rock" ,"Paper", "Scissors"];
-    return CHOICES[Math.floor(Math.random()*3)]
+    const CHOICES = ["Rock", "Paper", "Scissors"];
+    return CHOICES[Math.floor(Math.random() * 3)]
 }
 
+function playRound(playerSelection, computerSelection) {
+    let win = (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS")
+        || (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER")
+        || (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") ;
+    let draw = playerSelection.toUpperCase() === computerSelection.toUpperCase() ;
+
+    let lose = !win && !draw ;
+
+    if (win){
+        return `You win ! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()} !!`
+    }
+    else if (lose) {
+        return `You lose ! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()} !!`
+    }
+    else{
+        return 'It\'s a DRAW' ;
+    }
+
+}
