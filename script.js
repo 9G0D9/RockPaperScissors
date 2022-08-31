cdd020ae49b0eaf270789fa620ac2a9f3c77519f
Ghassen <dhiebghassen@gmail.com>
2022-08-31 14:39:46 +0100

    Add buttons for player selection

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function getComputerChoice(CHOICES) {
 
 function getPlayerChoice() {
     let playerSelection = prompt('Select a weapon', '');
-    
-    if (playerSelection==null) return ;
+
+    if (playerSelection == null) return;
     while (!isInArray(playerSelection, CHOICES)) {
         playerSelection = prompt('Weapon not available \nSelect a weapon', '');
-        if (playerSelection==null) return ;
+        if (playerSelection == null) return;
     }
     return playerSelection;
 }
@@ -53,50 +53,59 @@ function isInArray(playerSelection, CHOICES) {
     return CHOICES.indexOf(playerSelection.toUpperCase()) > -1;
 }
 
-function game(CHOICES) {
+// function game(CHOICES) {
 
-    let computerScore = 0;
-    let playerScroe = 0;
+//     let computerScore = 0;
+//     let playerScroe = 0;
 
-    for (let i = 0; i < 5; i++) {
-        
-
-        let computerSelection = getComputerChoice(CHOICES);
-        let playerSelection = getPlayerChoice();
-
-        if (playerSelection==null) return ;
+//     for (let i = 0; i < 5; i++) {
 
 
-        let result = getResultOfRound(playerSelection, computerSelection);
+//         let computerSelection = getComputerChoice(CHOICES);
+//         let playerSelection = getPlayerChoice();
 
-        if (result === "win") {
-            playerScroe += 1;
-        }
-        if (result === "lose") {
-            computerScore += 1;
-        }
-        if (result === "draw") {
-            i -= 1;
-        }
-
-        console.log(playRound(playerSelection, computerSelection));
-
-        console.log(`COMPUTER : ${computerScore}           PLAYER: ${playerScroe}`);
-
-        if (computerScore===3){
-            console.log('You lose !')
-            return ;
-        }
-        if (playerScroe ===3){
-            console.log('CONGRATS ! You win !!!')
-            return ;
-        }
-
-    }
+//         if (playerSelection==null) return ;
 
 
-}
-game(CHOICES);
+//         let result = getResultOfRound(playerSelection, computerSelection);
+
+//         if (result === "win") {
+//             playerScroe += 1;
+//         }
+//         if (result === "lose") {
+//             computerScore += 1;
+//         }
+//         if (result === "draw") {
+//             i -= 1;
+//         }
+
+//         console.log(playRound(playerSelection, computerSelection));
+
+//         console.log(`COMPUTER : ${computerScore}           PLAYER: ${playerScroe}`);
+
+//         if (computerScore===3){
+//             console.log('You lose !')
+//             return ;
+//         }
+//         if (playerScroe ===3){
+//             console.log('CONGRATS ! You win !!!')
+//             return ;
+//         }
+
+//     }
+
+
+//}
+const rockSelction = document.querySelector('.ROCK');
+const paperSelction = document.querySelector('.PAPER');
+const scissorsSelction = document.querySelector('.SCISSORS');
+
+rockSelction.addEventListener("click", ()=> console.log(playRound('ROCK', getComputerChoice(CHOICES))));
+paperSelction.addEventListener('click', ()=>console.log(playRound('PAPER', getComputerChoice(CHOICES))))
+scissorsSelction.addEventListener('click', ()=>console.log(playRound('SCISSORS', getComputerChoice(CHOICES))))
+
+
+
 
 
 
